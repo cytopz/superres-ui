f9aa04d7b1ce1e165f4351be8a214014a85a2a27
cytopz <cytopz@protonmail.com>
2021-01-09 16:37:11 +0700

    change navbar icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,14 @@
 <template>
   <nav>
     <v-app-bar color="white">
+      <img
+        :src="require('../assets/amiya.png')"
+        alt="amiya"
+        class="mr-3"
+        width="40"
+      />
       <v-toolbar-title class="text-overline">
         <span class="font-weight-bold">superres-upscaling</span>
-        <v-icon> mdi-chevron-double-up </v-icon>
       </v-toolbar-title>
     </v-app-bar>
   </nav>
